304d7f8ebd88f400a3e194f18ed166ae6b90ca4a
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-09 14:54:09 +0300

    feat: create views, setup URLs and base URLs configuration

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,10 @@
+# /var/www/Django-Ecommerce-Website/ecommerce/store/urls.py
+from django.urls import path
+from . import views # This imports functions from store/views.py
+
+urlpatterns = [
+    # Leave as empty string for base url of the store
+    path("", views.store, name="store"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
+]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,18 @@
+# /var/www/Django-Ecommerce-Website/ecommerce/store/views.py
 from django.shortcuts import render
+from django.http import HttpResponse # For simple text responses
 
-# Create your views here.
+def store(request):
+    # You'll eventually render a template here, e.g.,
+    # return render(request, 'store/store.html')
+    return HttpResponse("<h1>Welcome to the Store Page!</h1>")
+
+def cart(request):
+    # return render(request, 'store/cart.html')
+    return HttpResponse("<h1>Your Shopping Cart</h1>")
+
+def checkout(request):
+    # return render(request, 'store/checkout.html')
+    return HttpResponse("<h1>Checkout Page</h1>")
+
+# Add any other views your store app might need
