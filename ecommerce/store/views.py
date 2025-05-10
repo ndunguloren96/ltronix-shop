94e930ddfc55e56131353d864959ea44a3ffb326
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 19:27:03 +0300

    feat: Handle empty cart for non-authenticated users in checkout

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -24,5 +24,15 @@ def cart(request):
 
 
 def checkout(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create Empty car for now for none-logged in users
+        items = []
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+
     context = {}
     return render(request, "store/checkout.html", context)
