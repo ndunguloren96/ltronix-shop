6963cb93659fc157f6dd64c7f2bdb02785a3d031
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 11:20:50 +0300

    feat: add media root to set path for all media files and allow upload

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "static/images"
+)  # Media root will set a path for all media files to be uploaded to.
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
