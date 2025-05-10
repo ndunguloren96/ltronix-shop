96a99a9eb0dbf1f1dcdb3280ceba4cdd35b75ee3
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 07:55:26 +0300

    feat: add product model to define the schema

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -2,12 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+"""
+Each model maps to a single database table.
 
-# Customer Model- 
+"""
+
+
+# Customer Model
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# Product Model
+"""
+Digital will be true or false. Ltes us know if its a digital product or a physical product that needs to be shipped.
+"""
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
