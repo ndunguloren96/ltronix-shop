9988868fe5af1eb5c5b9b1f5c6182af7e2ccc3a0
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 18:45:13 +0300

    feat: Add get_total property to calculate item total

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -64,6 +64,11 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
 
 # Shipping Address Model
 """
