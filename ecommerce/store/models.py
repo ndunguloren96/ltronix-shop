d62375a308ef921ddce6242fcd66ab63dfb10a9a
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 07:59:13 +0300

    feat: implement order item model data structure

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -44,3 +44,11 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+# OrderItem Model
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
