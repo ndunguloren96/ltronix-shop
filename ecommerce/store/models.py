ac5de54c3ef5649e96cd481bc7ffb1aa9c03c2af
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 07:57:16 +0300

    feat: implement order model schema

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -31,3 +31,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# Order Model
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
