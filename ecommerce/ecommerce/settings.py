5982fc94e790570d8fcfadd8b889ec118a49b3e4
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 11:50:02 +0300

    feat: add MEDIA_URL

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+MEDIA_URL = "/images/"
+
 MEDIA_ROOT = os.path.join(
     BASE_DIR, "static/images"
 )  # Media root will set a path for all media files to be uploaded to.
