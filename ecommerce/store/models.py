ccabf0c16158f1a12752130ad23c481ff3087beb
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-10 08:17:30 +0300

    feat: implement shipping address data structure

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -52,3 +52,21 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+
+# Shipping Address Model
+"""
+Shipping Model is a child to order and will only be created if at least one orderitem within an order is a physical product 
+(If Product.digital == False).
+""" 
+class ShippingAddress(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	address = models.CharField(max_length=200, null=False)
+	city = models.CharField(max_length=200, null=False)
+	state = models.CharField(max_length=200, null=False)
+	zipcode = models.CharField(max_length=200, null=False)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.address
