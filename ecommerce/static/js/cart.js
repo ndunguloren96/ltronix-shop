bbc0dd7c5d0d89e9ba9acc80d64695ae7a2965d6
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-11 19:20:25 +0300

    feat: add shipping logic

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,39 +1,80 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart');
 
-for (i = 0; i < updateBtns.length; i++) {
-    updateBtns[i].addEventListener('click', function () {
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function() {
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
 
-        console.log('USER:', user)
-        if (user === 'AnonymousUser') {
-            console.log('Not logged in')
-            // addCookieItem(productId, action)
-        } else {
-            updateUserOrder(productId, action)
-        }
+        console.log('productId:', productId, 'Action:', action);
 
-    })
-
+        fetch('/update_item/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken'),
+            },
+            body: JSON.stringify({
+                'productId': productId,
+                'action': action
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Data:', data);
+            updateCartDisplay(productId, action, data); // Call function to update display
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred while updating the cart.');
+        });
+    });
 }
 
-function updateUserOrder(productId, action){
-    console.log('User is logged in, sending data...')
-        var url = '/update_item'
-        
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken, // CSRF token for security. Pass 'csrftoken' variable set to our fetch() call
-        },
-        body: JSON.stringify({ 'productId': productId, 'action': action })
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            location.reload()
-        });
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var parts = document.cookie.split(';');
+        for (var i = 0; i < parts.length; i++) {
+            var cookie = parts[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+function updateCartDisplay(productId, action, data) {
+    var quantityElement = document.querySelector(`.quantity[data-product="${productId}"]`);
+    var totalElement = document.querySelector(`.total[data-product="${productId}"]`);
+    var cartItemsElement = document.querySelector(`.cart-items`); //select the element displaying the total cart items
+    var cartTotalElement = document.querySelector(`.cart-total`);  //element displaying the cart total
+
+
+    if (quantityElement) {
+        if (action === 'add') {
+            quantityElement.textContent = 'x' + data.quantity;
+        } else if (action === 'remove') {
+             if(data.quantity > 0){
+                 quantityElement.textContent = 'x' + data.quantity;
+             }
+             else{
+                quantityElement.parentElement.parentElement.remove();
+             }
+        }
+    }
+
+    if (totalElement) {
+        totalElement.textContent = 'Ksh ' + data.total_price;
+    }
+    if(cartItemsElement){
+        cartItemsElement.textContent = data.cart_items;
+    }
+
+    if(cartTotalElement){
+        cartTotalElement.textContent = 'Ksh' + data.cart_total;
+    }
 }
