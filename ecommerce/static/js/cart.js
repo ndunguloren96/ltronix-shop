a153ddb3a3f39a5c3a293d4c3b70a1ac67c8e904
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-11 08:44:24 +0300

    feat: use typr logic to check status on click

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -6,6 +6,15 @@ for (i = 0; i < updateBtns.length; i++) {
         var action = this.dataset.action
         console.log('productId:', productId, 'Action:', action)
 
+        console.log('USER:', user)
+        if (user === 'AnonymousUser') {
+            console.log('Not logged in')
+            // addCookieItem(productId, action)
+        } else {
+            console.log('User is logged in, sending data...')
+            // updateUserOrder(productId, action)
+        }
+
     })
 
 }
