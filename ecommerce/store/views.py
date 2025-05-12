298594ea3dc1ef1adaacb7427430df58d554dfc2
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-12 12:14:49 +0300

    feat: add a try/except to handle instance where product does not exist

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -39,19 +39,28 @@ def cart(request):
         cartItems = order['get_cart_items']
 
         for i in cart: # i = product id
-            cartItems += cart[i]['quantity']
+            # We use try block to prevent items in cart that may have been removed from causing errors
+            try:
+                cartItems += cart[i]['quantity']
 
-            product = Product.objects.get(id=i)
-            total = (product.price * cart[i]['quantity'])
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
 
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]['quantity']
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
 
-            items.append(item)
+                items = {
+                    'id' :product.id,
+                    'product':{'id':product.id, 'name':product.name, 'price':product.price, 'imageURL':product.imageURL}, 'quantity':cart[i]['quantity'], 'digital':product.digital, 'get_total':total,
+                }
 
-            if product.digital == False:
-                order['shipping'] = True
+                items.append(item)
 
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass
+            
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
