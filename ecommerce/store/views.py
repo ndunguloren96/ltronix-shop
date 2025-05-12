ac24775a53cb49a6d47a6b5106061bb9b4fb0465
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-12 07:59:45 +0300

    feat: query cart for non-logged in users in views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -28,9 +28,16 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         # Create empty cart for now for non-logged in user
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print('Cart:', cart)
+        
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
+
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
