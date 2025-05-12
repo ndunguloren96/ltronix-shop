cc8fd67c0ae6eeb6b6f090dd9b3db075cd5e2033
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-12 16:33:08 +0300

    Revert "feat: clean up process order view and add to import"
    This reverts commit bb79546fa1aaf634f7570b7e7e258ec0033a38d2.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 import json
 import datetime
 from .models import * 
-from .utils import cookieCart, cartData, guestOrder
+from .utils import cookieCart, cartData
 
 def store(request):
 	data = cartData(request)
@@ -69,8 +69,35 @@ def processOrder(request):
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
 	else:
-		customer, order = guestOrder(request, data)
+		print('User is not logged in')
+
+		print('COOKIES:', request.COOKIES)
+		name = data['form']['name']
+		email = data['form']['email']
+
+		cookieData = cookieCart(request)
+		items = cookieData['items']
+
+		customer, created = Customer.objects.get_or_create(
+				email=email,
+				)
+		customer.name = name
+		customer.save()
+
+		order = Order.objects.create(
+			customer=customer,
+			complete=False,
+			)
+
+		for item in items:
+			product = Product.objects.get(id=item['id'])
+			orderItem = OrderItem.objects.create(
+				product=product,
+				order=order,
+				quantity=item['quantity'],
+			)
 
 	total = float(data['form']['total'])
 	order.transaction_id = transaction_id
