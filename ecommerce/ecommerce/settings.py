5c6171592d383560f5bdcbf26783009b39f67af5
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-13 09:33:12 +0300

    fix: Load Django SECRET_KEY from environment

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = Env()
 env.read_env(os.path.join(BASE_DIR, '.env')) # read .evn file
 
+SECRET_KEY=env('SECRET_KEY')
+
 CONSUMER_KEY = env('SAFARICOM_CONSUMER_KEY')
 CONSUMER_SECRET = env('SAFARICOM_CONSUMER_SECRET')
 
