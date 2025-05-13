15e1da75d6a0993e46630e4e3ba181e7bff8eed9
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-13 08:00:20 +0300

    feat: configure database and install and configure dotenv

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,11 +80,16 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': os.getenv('DATABASE_ENGINE'),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),  # Provide a default empty string for security if not set
+        'HOST': os.getenv('DATABASE_HOST', 'localhost'),  # Provide a default 'localhost'
+        'PORT': os.getenv('DATABASE_PORT', '5432'),      # Provide a default port '5432'
     }
 }
+
 
 
 # Password validation
