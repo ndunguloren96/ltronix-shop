10493462e4ad117f6ecdf23f5bbbe7e371767482
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-13 09:35:35 +0300

    add: create payment app urls.py file and initial configuration

---
+++ ecommerce/payment/urls.py
@@ -0,0 +1,8 @@
+# payment/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Define your payment app's URLs here
+    # path('some/payment/url/', views.some_view, name='some_payment_name'),
+]
