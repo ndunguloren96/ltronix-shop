43ebe0487ee54d8ff8099dd6688e8234c467767e
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-14 11:59:23 +0300

    Feat: Implement M-Pesa STK Push and callback handling

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -25,7 +25,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('payment/', include('payment.urls')),
+    path('', include('payment.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
