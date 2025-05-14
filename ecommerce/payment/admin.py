1a85c9f5046b6f1b70363d65dae2d9b9b4c545fe
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-14 21:40:28 +0300

    feat: Admin Transaction Logging.  To make the transactions visible in the Django admin interface

--- ecommerce/payment/admin.py
+++ ecommerce/payment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Transaction  # Import the Transaction model
 
 # Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'phone', 'amount', 'status', 'created_at']
+    search_fields = ['phone', 'checkout_request_id', 'merchant_request_id']
