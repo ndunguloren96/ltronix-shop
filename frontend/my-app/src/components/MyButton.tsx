f36cbe818223e69d8d6ea7dc083a17331e4fbce5
ndunguloren96 <ndunguloren96@gmail.com>
2025-05-20 21:08:06 +0300

    feat: Button component build and test in isolation

---
+++ frontend/my-app/src/components/MyButton.tsx
@@ -0,0 +1,17 @@
+// src/components/MyButton.tsx
+'use client'; // This component will be a client component
+
+import { Button, ButtonProps } from '@chakra-ui/react';
+import React from 'react';
+
+interface MyButtonProps extends ButtonProps {
+  children: React.ReactNode;
+}
+
+export const MyButton: React.FC<MyButtonProps> = ({ children, ...props }) => {
+  return (
+    <Button {...props} colorScheme="brand">
+      {children}
+    </Button>
+  );
+};
