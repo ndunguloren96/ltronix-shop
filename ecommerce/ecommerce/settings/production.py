6ace4f032712b95372f0801e6ad82e4c82cd4ba2
ndunguloren96 <ndunguloren96@gmail.com>
2025-07-04 16:23:15 +0300

    fix(config): Resolve f-string syntax error in production settings

--- ecommerce/ecommerce/settings/production.py
+++ ecommerce/ecommerce/settings/production.py
@@ -40,7 +40,7 @@ AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME') # e.g., 'us-east-1'
-AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN', default=f'{env('AWS_STORAGE_BUCKET_NAME')}.s3.amazonaws.com')
+AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN', default=f"{env('AWS_STORAGE_BUCKET_NAME')}.s3.amazonaws.com")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None # Or 'public-read' if files should be publicly accessible
 
