b03108657a42d9cf028d83057d672fa9e370d0e0
Loren Ndungu <ndunguloren96@gmail.com>
2025-07-08 17:56:50 +0300

    Update chakra.config.ts to wrap with extendTheme

--- frontend/my-app/src/chakra.config.ts
+++ frontend/my-app/src/chakra.config.ts
@@ -1,6 +1,10 @@
-import { type ThemeConfig } from '@chakra-ui/react';
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 export const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 };
+
+const theme = extendTheme({ config });
+
+export default theme;
