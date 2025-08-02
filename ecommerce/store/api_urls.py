e10d22596d9f6117f0e69a0f50b9d6df88b8ec9f
Loren Ndungu <ndunguloren96@gmail.com>
2025-08-02 13:40:24 +0300

    feat: Add /api/v1/orders/ endpoint

--- ecommerce/store/api_urls.py
+++ ecommerce/store/api_urls.py
@@ -1,3 +1,4 @@
+# ecommerce/store/api_urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import api_views
@@ -8,4 +9,6 @@ router.register(r'carts', api_views.CartViewSet, basename='cart')
 
 urlpatterns = [
     path('', include(router.urls)),
+    # --- NEW PATH FOR /api/v1/orders/ ---
+    path('orders/', api_views.OrderItemCreateAPIView.as_view(), name='api-add-to-cart'),
 ]
